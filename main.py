bf1912e342d200483a69bd7e6dea4ee2a69b4dea
wattox00 <korschzozinho@gmail.com>
2026-03-23 17:14:56 +0100

    Basic TUI sound player

--- main.py
+++ main.py
@@ -1,1 +1,62 @@
 import vlc
+import time
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Static
+from textual.containers import Container
+from textual.binding import Binding
+
+song = ''
+
+def song_playing():
+    player = vlc.MediaPlayer(song)
+    return player
+
+player = song_playing()
+
+class MyApp(App):
+    """Lysn"""
+
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+
+    #box {
+        border: round green;
+        padding: 1 2;
+        width: 50%;
+    }
+    """
+
+    BINDINGS = [
+        Binding("q", "quit", "Quit"),
+        Binding("e", "playsong", "Play Song"),
+        Binding("s", "stopsong", "Stop Song"),
+        Binding("p", "pausesong", "Pause Song"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        """Create UI layout."""
+        yield Header()
+        with Container(id="box"):
+            self.text = Static("Lysn")
+            yield self.text
+        yield Footer()
+
+    def action_playsong(self) -> None:
+        self.text.update(f"song playing: {song}.")
+        player = song_playing()
+        player.play()
+
+    def action_stopsong(self) -> None:
+        self.text.update(f"song playing: {song}.")
+        player.stop()
+
+    def action_pausesong(self) -> None:
+        self.text.update(f"song playing: {song}.")
+        player.pause()
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.run()
+
