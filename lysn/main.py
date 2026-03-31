f9c9c9152fc1f77e98d9a670154123ab53c0de01
wattox00 <korschzozinho@gmail.com>
2026-03-31 17:55:26 +0200

    Bug fix

--- lysn/main.py
+++ lysn/main.py
@@ -159,9 +159,6 @@ https://github.com/wattox00/lysn
         self.pending_action = None
 
     def action_quit(self):
-        if self.input_mode:
-            # Ignore quit while typing
-            return
         self.exit()
 
     def get_active_tab(self):
